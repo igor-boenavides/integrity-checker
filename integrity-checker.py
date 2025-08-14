f3e40077261097ba2e1eca0ab2f25af5cac07f02
igor.boenavides <igor.boenavides@proton.me>
2025-08-14 20:36:26 -0300

    Added exception raising.

--- integrity-checker.py
+++ integrity-checker.py
@@ -5,21 +5,30 @@ def main():
     original_hash = input("Inform the original hash: ")
     hash_algorithm = int(input("Inform the hash algorithm (md5 = 1, sha256 = 2: "))
 
-    with open(sys.argv[1], 'rb') as f:
-        file = f.read()
+    while True:
+        try:
+            with open(sys.argv[1], 'rb') as f:
+                file = f.read()
 
-    if hash_algorithm == 1:
-        hashed_file = hashlib.md5(file).hexdigest()
-    elif hash_algorithm == 2:
-        hashed_file = hashlib.sha256(file).hexdigest()
-    else:
-        print("Valor inválido.")
-        exit()
-
-    if original_hash != hashed_file:
-        print("Wrong!")
-    else:
-        print("Good.")
+            if hash_algorithm == 1:
+                hashed_file = hashlib.md5(file).hexdigest()
+            elif hash_algorithm == 2:
+                hashed_file = hashlib.sha256(file).hexdigest()
+            else:
+                print("Valor inválido.")
+                exit()
+        except IndexError:
+            print("Must inform a file.")
+            exit()
+        except FileNotFoundError:
+            print("Must inform a valid file.")
+            exit()
+        else:
+            if original_hash != hashed_file:
+                print("Wrong!")
+            else:
+                print("Good.")
+            break
 
 if __name__ == "__main__":
     main()
